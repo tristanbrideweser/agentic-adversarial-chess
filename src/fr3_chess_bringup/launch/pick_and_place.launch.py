f70f8ff185079ac68fd849b460c63d04bd614cb4
Tristan Brideweser <tbridewe@purdue.edu>
2026-03-13 20:06:38 -0400

    ignoring pycache and ros build

---
+++ src/fr3_chess_bringup/launch/pick_and_place.launch.py
@@ -0,0 +1,99 @@
+import os
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    # Package directories
+    description_pkg_share = FindPackageShare('fr3_chess_description')
+    sim_pkg_share = FindPackageShare('fr3_chess_sim')
+    bringup_pkg_share = FindPackageShare('fr3_chess_bringup')
+    ros_gz_sim_pkg_share = FindPackageShare('ros_gz_sim')
+
+    # Path to the xacro file
+    xacro_file = PathJoinSubstitution([description_pkg_share, 'urdf', 'fr3_chess.urdf.xacro'])
+
+    # Robot description
+    # Pass the path to the controller config to the xacro if needed, 
+    # but the xacro already has a hardcoded path we might want to override or just use.
+    # Actually, the plugin in the xacro expects a path.
+    robot_description = Command(['xacro ', xacro_file])
+
+    # Gazebo World
+    world_file = PathJoinSubstitution([sim_pkg_share, 'worlds', 'chess_world.sdf'])
+
+    # Environment variable for Gazebo OGRE crash on some systems
+    force_sw = SetEnvironmentVariable("LIBGL_ALWAYS_SOFTWARE", "1")
+
+    # Launch Gazebo
+    gz_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution([ros_gz_sim_pkg_share, 'launch', 'gz_sim.launch.py'])
+        ),
+        launch_arguments={'gz_args': ['-r -v 4 ', world_file]}.items()
+    )
+
+    # Robot State Publisher
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_description}]
+    )
+
+    # Spawn Robot
+    spawn_robot = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[
+            '-name', 'fr3_chess',
+            '-topic', 'robot_description',
+            '-x', '0.0',
+            '-y', '0.0',
+            '-z', '0.78'
+        ],
+        output='screen'
+    )
+
+    # Bridge for clock
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=['/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock'],
+        output='screen'
+    )
+
+    # Spawners for controllers
+    joint_state_broadcaster_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['joint_state_broadcaster'],
+    )
+
+    arm_controller_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['fr3_arm_controller'],
+    )
+
+    hand_controller_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['fr3_hand_controller'],
+    )
+
+    return LaunchDescription([
+        force_sw,
+        gz_sim,
+        robot_state_publisher,
+        spawn_robot,
+        bridge,
+        joint_state_broadcaster_spawner,
+        arm_controller_spawner,
+        hand_controller_spawner
+    ])
--- src/fr3_chess_bringup/setup.py
+++ src/fr3_chess_bringup/setup.py
@@ -12,7 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yaml'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -23,6 +24,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'simple_pick_place = fr3_chess_bringup.simple_pick_place:main',
         ],
     },
 )
